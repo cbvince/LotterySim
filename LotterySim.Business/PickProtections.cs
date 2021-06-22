3963e3ca3c52f9eecd7a99e8abb2646c00b46c37
Chris Vincent <cvincent@starrez.com>
2021-06-22 08:53:08 -0600

    Added Styling to Detail Page Updated Pick Protection scenarios for OKC swaps

--- LotterySim.Business/PickProtections.cs
+++ LotterySim.Business/PickProtections.cs
@@ -10,36 +10,83 @@ namespace LotterySim.Business
     {
         public static void PickProtection(List<Team> teams)
         {
-            DetermineProtection(teams, 4, "Houston", "Oklahoma City");
+            //DetermineProtection(teams, 4, "Houston", "Oklahoma City");
             DetermineProtection(teams, 3, "Minnesota", "Golden State");
             DetermineProtection(teams, 4, "Chicago", "Orlando");
             DetermineProtection(teams, 14, "Portland", "Houston");
             DetermineProtection(teams, 9, "Milwaukee", "Houston");
             DetermineProtection(teams, 0, "Dallas", "New York");
             DetermineProtection(teams, 16, "Detroit", "Houston");
-            DetermineProtection(teams, 0, "Boston", "Oklahoma City");
+            //DetermineProtection(teams, 0, "Boston", "Oklahoma City");
+            DetermineProtectionOKCSwaps(teams);
         }
 
 
         private static void DetermineProtection(List<Team> teams, int protectionThreshold, string originalTeam, string newTeam)
         {
-           
+
             var teamToConvey = teams.FirstOrDefault(p => p.OriginalTeamName == originalTeam);
 
             if (teamToConvey.PickNumber > protectionThreshold)
 
             {
-                teamToConvey.TeamName = string.Format("{0} to {1}", originalTeam, newTeam);      
+                teamToConvey.TeamName = string.Format("{0} to {1}", originalTeam, newTeam);
                 teamToConvey.NewTeamName = newTeam;
             }
 
-           
+
             else
             {
-               teamToConvey.TeamName = originalTeam;
+                teamToConvey.TeamName = originalTeam;
                 teamToConvey.NewTeamName = null;
             }
-           
+
+        }
+
+        //OKC gets 3 best of OKC/HOU/MIA, HOU gets worst & can swap with BKN
+        private static void DetermineProtectionOKCSwaps(List<Team> teams)
+        {
+            var houston = teams.FirstOrDefault(p => p.OriginalTeamName == "Houston");
+            var okc = teams.FirstOrDefault(p => p.OriginalTeamName == "Oklahoma City");
+            var miami = teams.FirstOrDefault(p => p.OriginalTeamName == "Miami");
+            var boston = teams.FirstOrDefault(p => p.OriginalTeamName == "Boston");
+            var highestPickSwapTeam = teams.FirstOrDefault(p => p.OriginalTeamName == "Boston" || p.OriginalTeamName == "Houston" || p.OriginalTeamName == "Oklahoma City" || p.OriginalTeamName == "Miami");
+            var lowestPickSwapTeam =
+                teams.Where(p => p.OriginalTeamName == "Boston" || p.OriginalTeamName == "Houston" || p.OriginalTeamName == "Oklahoma City" || p.OriginalTeamName == "Miami")
+                .OrderByDescending(x => x.PickNumber).FirstOrDefault();
+
+
+            if (houston.PickNumber < 4)
+            {
+                foreach (var team in teams.Where(p => p.OriginalTeamName == "Boston" || p.OriginalTeamName == "Oklahoma City" || p.OriginalTeamName == "Miami"))
+                {
+                    SwapPicks(team, okc);
+                }
+            }
+
+            else
+            {
+                foreach (var team in teams.Where(p => p.OriginalTeamName == "Boston" || p.OriginalTeamName == "Oklahoma City" || p.OriginalTeamName == "Miami" || p.OriginalTeamName == "Houston"))
+                {
+                    SwapPicks(team, okc);
+                    
+                }
+                SwapPicks(lowestPickSwapTeam, houston);
+            }
+
+
+        }
+
+        private static void SwapPicks(Team fromteam, Team toteam)
+        {
+            if (fromteam == toteam)
+            {
+                return;
+            }
+            
+            
+            fromteam.NewTeamName = toteam.OriginalTeamName;
+            fromteam.TeamName = string.Format("{0} to {1}", fromteam.OriginalTeamName, toteam.OriginalTeamName);
         }
         
         
