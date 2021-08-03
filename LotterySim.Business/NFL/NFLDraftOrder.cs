6cf061aca9a4f64888d13b4106b8144b1a276eee
Chris V <cbvince@gmail.com>
2021-08-02 22:13:58 -0600

    Added methods to order nfl teams

--- LotterySim.Business/NFL/NFLDraftOrder.cs
+++ LotterySim.Business/NFL/NFLDraftOrder.cs
@@ -8,27 +8,46 @@ namespace LotterySim.Business.NFL
 {
     class NFLDraftOrder
     {
-       private static void GetNonPlayoffTeams(List<NFLTeam> teams)
+       
+        public static List<NFLTeam.Entry> GetNonPlayOffTeams(List<NFLTeam.Entry> teams)
         {
+            return GetNFLTeamDraftGroup(teams, 12, 32);
+        }
+
+        public static List<NFLTeam.Entry> GetWildCardRoundLoserTeams(List<NFLTeam.Entry> teams)
+        {
+            return GetNFLTeamDraftGroup(teams, 6, 12);
+        }
+
+        public static List<NFLTeam.Entry> GetDivisionRoundLoserTeams(List<NFLTeam.Entry> teams)
+        {
+            return GetNFLTeamDraftGroup(teams, 4, 6);
         }
 
 
-        private static void GetPlayoffTeams(List<NFLTeam> teams)
-        {
-        }
 
-        private static List<NFLTeam.Entry> GetAFCTeams(List<NFLTeam.Entry> teams)
-        {
-            var afcTeams = new List<NFLTeam.Entry>();
 
-            foreach (var team in teams)
+
+
+        private static List<NFLTeam.Entry> GetNFLTeamDraftGroup(List<NFLTeam.Entry> teams, int lowerSeedThreshold, int upperSeedThreshold)
+        {
+            var teamsDraftGroup = new List<NFLTeam.Entry>();
+            foreach (var team in teams.Where(p => GetTeamPlayoffSeed(p.stats) > lowerSeedThreshold && GetTeamPlayoffSeed(p.stats) <= upperSeedThreshold))
             {
-                afcTeams.Add(team);
-                var teamStats = team.stats;
-                bool playoffTeam = teamStats.Exists(p => p.displayName == "playoffSeed" && p.value < 8);
+                teamsDraftGroup.Add(team);
             }
 
-            return afcTeams;
+            return teamsDraftGroup;
+        }
+
+
+
+       
+
+        private static int GetTeamPlayoffSeed(List<NFLTeam.Stat> stats)
+        {
+            var playoffSeed = stats.Where(p => p.name == "playoffSeed").FirstOrDefault().value;
+            return playoffSeed;
         }
 
     }
