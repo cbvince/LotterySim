2c021b16edfea5ca942063fceb24c76204683b29
Chris V <cbvince@gmail.com>
2021-08-14 07:57:37 -0600

    Moved GamesBack Calculation to Common Methods Moved Error Page to Shared

---
+++ LotterySim.Business/Common/GenericStandingDataMethods.cs
@@ -0,0 +1,21 @@
+﻿using LotterySim.Business.NFL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotterySim.Business.Common
+{
+    class GenericStandingDataMethods
+    {
+        public static int GetGamesBack(int highestNumberOfLosses, int lowestNumberOfWins, int teamWinLossDifference)
+        {
+            var winLostDifferenceForWorstTeam = highestNumberOfLosses - lowestNumberOfWins;
+            var gamesBack = (winLostDifferenceForWorstTeam - teamWinLossDifference) / 2;
+            return gamesBack;
+        }
+
+      
+    }
+}
